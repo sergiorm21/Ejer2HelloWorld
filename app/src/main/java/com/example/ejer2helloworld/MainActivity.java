16616a980bc1d48867378a944dcbde158c5464f7
sergiorm21 <reina.moser21@cordoba.salesianos.edu>
2022-10-11 20:08:55 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ejer2helloworld/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.ejer2helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    TextView textView;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+
+
+
+
+
+
+    }
+}
